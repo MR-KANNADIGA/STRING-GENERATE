6a0581116aef9ac5526b38cd1d123c7a1a727932
ᴹᴿ KANNADIGA <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-03-28 11:26:17 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.get_me().username
-    print(f"@{uname} Started Successfully!")
+    print(f"@{uname} Started Successfully ab isko join kar @Karunada_Kings_and_Queens!")
     idle()
     app.stop()
     print("Bot stopped. Alvida!")
